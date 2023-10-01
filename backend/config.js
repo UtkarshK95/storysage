7d35fa3a74a12e9400a18aeff646e02e9b4a75c4
Utkarsh Katiyar <utkarsh_katiyar@outlook.com>
2023-10-01 19:31:24 +0530

    Backend Completed

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+//   root:root is the username:password
+//   books-collection is added in the connection string which is the name of the database
+export const mongoDBURL =
+  "mongodb+srv://root:root@storysage-mern.8wqzdgn.mongodb.net/books-collection?retryWrites=true&w=majority";
